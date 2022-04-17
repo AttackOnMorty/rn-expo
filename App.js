cc9566ea7f57d231ca427c057b62122c06a6053f
pfmao <ErenJagerM@outlook.com>
2022-04-17 17:04:45 +0800

    Extract ColorBox component

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { SafeAreaView, Text, View, StyleSheet } from 'react-native';
+import ColorBox from './components/ColorBox';
 
 const App = () => {
   return (
@@ -8,18 +9,10 @@ const App = () => {
         <Text style={styles.heading}>
           Here are some boxes of different colours
         </Text>
-        <View style={[styles.box, styles.cyan]}>
-          <Text style={styles.text}>Cyan #2aa198</Text>
-        </View>
-        <View style={[styles.box, styles.blue]}>
-          <Text style={styles.text}>Blue #268bd2</Text>
-        </View>
-        <View style={[styles.box, styles.magenta]}>
-          <Text style={styles.text}>Magenta #d33682</Text>
-        </View>
-        <View style={[styles.box, styles.orange]}>
-          <Text style={styles.text}>Orange #cb4b16</Text>
-        </View>
+        <ColorBox colorName="Cyan" hexCode="#2aa198" />
+        <ColorBox colorName="Blue" hexCode="#268bd2" />
+        <ColorBox colorName="Magenta" hexCode="#d33682" />
+        <ColorBox colorName="Orange" hexCode="#cb4b16" />
       </View>
     </SafeAreaView>
   );
@@ -34,29 +27,6 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: 'bold',
     marginBottom: 10,
-  },
-  box: {
-    padding: 10,
-    borderRadius: 3,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginBottom: 10,
-  },
-  text: {
-    fontWeight: 'bold',
-    color: 'white',
-  },
-  cyan: {
-    backgroundColor: '#2aa198',
-  },
-  blue: {
-    backgroundColor: '#268bd2',
-  },
-  magenta: {
-    backgroundColor: '#d33682',
-  },
-  orange: {
-    backgroundColor: '#cb4b16',
   },
 });
 
---
+++ components/ColorBox.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+const ColorBox = ({ colorName, hexCode }) => (
+  <View style={[styles.box, { backgroundColor: hexCode }]}>
+    <Text style={styles.text}>
+      {colorName} {hexCode}
+    </Text>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  box: {
+    padding: 10,
+    borderRadius: 3,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  text: {
+    fontWeight: 'bold',
+    color: 'white',
+  },
+});
+
+export default ColorBox;
